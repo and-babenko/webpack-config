6d3d431d2c643ae5d9086ed3681d8e94f2fdafc0
Andrey Babenko <and.babenko96@gmail.com>
2022-11-20 23:06:35 +0200

    better structure, scss url fix, more actual plugins applied

---
+++ dist/js/app.js
@@ -0,0 +1,254 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./index.html":
+/*!********************!*\
+  !*** ./index.html ***!
+  \********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/html-loader/dist/runtime/getUrl.js */ "../node_modules/html-loader/dist/runtime/getUrl.js");
+/* harmony import */ var _node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0__);
+// Imports
+
+var ___HTML_LOADER_IMPORT_0___ = new URL(/* asset import */ __webpack_require__(/*! ./assets/favicon/favicon.ico */ "./assets/favicon/favicon.ico"), __webpack_require__.b);
+var ___HTML_LOADER_IMPORT_1___ = new URL(/* asset import */ __webpack_require__(/*! ./assets/imgs/1.jpg */ "./assets/imgs/1.jpg"), __webpack_require__.b);
+var ___HTML_LOADER_IMPORT_2___ = new URL(/* asset import */ __webpack_require__(/*! ./assets/imgs/hero.jpg */ "./assets/imgs/hero.jpg"), __webpack_require__.b);
+// Module
+var ___HTML_LOADER_REPLACEMENT_0___ = _node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0___default()(___HTML_LOADER_IMPORT_0___);
+var ___HTML_LOADER_REPLACEMENT_1___ = _node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0___default()(___HTML_LOADER_IMPORT_1___);
+var ___HTML_LOADER_REPLACEMENT_2___ = _node_modules_html_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_0___default()(___HTML_LOADER_IMPORT_2___);
+var code = "<!DOCTYPE html>\r\n<html lang=\"en\">\r\n  <head>\r\n    <meta charset=\"UTF-8\" />\r\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\r\n    <link rel=\"icon\" href=\"" + ___HTML_LOADER_REPLACEMENT_0___ + "\" />\r\n    <title>WebSite</title>\r\n  </head>\r\n  <body>\r\n    <div>Какой-то тестовый текст</div>\r\n    <img src=\"" + ___HTML_LOADER_REPLACEMENT_1___ + "\" style=\"width: 100px; height: 100px\" />\r\n    <img src=\"" + ___HTML_LOADER_REPLACEMENT_2___ + "\" style=\"width: 100px; height: 100px\" />\r\n    <div>еще какой-то текст</div>\r\n  </body>\r\n</html>\r\n";
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (code);
+
+/***/ }),
+
+/***/ "../node_modules/html-loader/dist/runtime/getUrl.js":
+/*!**********************************************************!*\
+  !*** ../node_modules/html-loader/dist/runtime/getUrl.js ***!
+  \**********************************************************/
+/***/ ((module) => {
+
+
+
+module.exports = function (url, options) {
+  if (!options) {
+    // eslint-disable-next-line no-param-reassign
+    options = {};
+  }
+
+  if (!url) {
+    return url;
+  } // eslint-disable-next-line no-underscore-dangle, no-param-reassign
+
+
+  url = String(url.__esModule ? url.default : url);
+
+  if (options.hash) {
+    // eslint-disable-next-line no-param-reassign
+    url += options.hash;
+  }
+
+  if (options.maybeNeedQuotes && /[\t\n\f\r "'=<>`]/.test(url)) {
+    return "\"".concat(url, "\"");
+  }
+
+  return url;
+};
+
+/***/ }),
+
+/***/ "./assets/favicon/favicon.ico":
+/*!************************************!*\
+  !*** ./assets/favicon/favicon.ico ***!
+  \************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "13992b70516a1052f690.ico";
+
+/***/ }),
+
+/***/ "./assets/imgs/1.jpg":
+/*!***************************!*\
+  !*** ./assets/imgs/1.jpg ***!
+  \***************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "assets/imgs/1.jpg";
+
+/***/ }),
+
+/***/ "./assets/imgs/hero.jpg":
+/*!******************************!*\
+  !*** ./assets/imgs/hero.jpg ***!
+  \******************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "assets/imgs/hero.jpg";
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl + "../";
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	(() => {
+/******/ 		__webpack_require__.b = document.baseURI || self.location.href;
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			"app": 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		// no on chunks loaded
+/******/ 		
+/******/ 		// no jsonp function
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
+(() => {
+var __webpack_exports__ = {};
+/*!*******************!*\
+  !*** ./js/app.js ***!
+  \*******************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _index_html__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../index.html */ "./index.html");
+// import "../scss/style.scss";
+
+var a = {
+  babenko: 5
+};
+})();
+
+// This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
+(() => {
+/*!*************************!*\
+  !*** ./scss/style.scss ***!
+  \*************************/
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=app.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -1,178 +1,203 @@
-//123123123
 const path = require("path"); // 2.1
 
 const HTMLWebpackPlugin = require("html-webpack-plugin"); // 8.1
-const { CleanWebpackPlugin } = require("clean-webpack-plugin"); // 9
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // 10.1
-const CopyWebpackPlugin = require("copy-webpack-plugin"); // 12.1
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // 9.1
+
 const OptimizeCssAssetWebpackPlugin = require("optimize-css-assets-webpack-plugin"); // 17.1
 const TerserWebpackPlugin = require("terser-webpack-plugin"); // 17.2
 const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
-const isDev = process.env.NODE_ENV === "development"; // 6.1
-const isProd = !isDev; // 6.2
+// 2.2
+const PATHS = {
+  src: path.resolve(__dirname, "src"),
+  dist: path.resolve(__dirname, "dist"),
+  devAssets: path.resolve(__dirname, "src/assets"),
+  prodAssets: path.resolve(__dirname, "dist/assets"),
+  html: path.resolve(__dirname, "src/index.html"),
+};
 
-const filename = (ext) => (isDev ? `[name]${ext}` : `[name].[hash]${ext}`); // 7.1
+// 1
+module.exports = (env, argv) => {
+  const isDev = argv.mode === "development"; // 6.1
+  const isProd = !isDev; // 6.2
 
-const optimization = () => {
-  // 17.4
-  const configObj = {
-    splitChunks: {
-      // 17.5
-      chunks: "all",
-    },
-  };
-  if (isProd) {
-    configObj.minimizer = [
-      new OptimizeCssAssetWebpackPlugin(), // 17.6
-      new TerserWebpackPlugin(), // 17.7
-      new ImageMinimizerPlugin({
-        minimizer: {
-          implementation: ImageMinimizerPlugin.imageminMinify,
-          options: {
-            plugins: [
-              ["gifsicle", { interlaced: true }],
-              ["jpegtran", { progressive: true }],
-              ["optipng", { optimizationLevel: 5 }],
-              [
-                "svgo",
-                {
-                  plugins: [
-                    {
-                      name: "preset-default",
-                      params: {
-                        overrides: {
-                          removeViewBox: false,
-                          addAttributesToSVGElement: {
-                            params: {
-                              attributes: [
-                                { xmlns: "http://www.w3.org/2000/svg" },
-                              ],
+  const filename = (ext) =>
+    isDev ? `[name]${ext}` : `[name].[hash].min${ext}`; // 6.3
+
+  const optimization = () => {
+    // 17.4
+    const configObj = {
+      splitChunks: {
+        // 17.5
+        chunks: "all",
+      },
+    };
+    if (isProd) {
+      configObj.minimizer = [
+        new OptimizeCssAssetWebpackPlugin(), // 17.6
+        new TerserWebpackPlugin(), // 17.7
+        new ImageMinimizerPlugin({
+          minimizer: {
+            implementation: ImageMinimizerPlugin.imageminMinify,
+            options: {
+              plugins: [
+                ["gifsicle", { interlaced: true }],
+                ["jpegtran", { progressive: true }],
+                ["optipng", { optimizationLevel: 5 }],
+                [
+                  "svgo",
+                  {
+                    plugins: [
+                      {
+                        name: "preset-default",
+                        params: {
+                          overrides: {
+                            removeViewBox: false,
+                            addAttributesToSVGElement: {
+                              params: {
+                                attributes: [
+                                  { xmlns: "http://www.w3.org/2000/svg" },
+                                ],
+                              },
                             },
                           },
                         },
                       },
-                    },
-                  ],
-                },
+                    ],
+                  },
+                ],
               ],
-            ],
-          },
-        },
-      }),
-    ];
-  }
-  return configObj;
-};
-
-// 1
-module.exports = {
-  mode: "development", // 6
-  context: path.resolve(__dirname, "src"), // 2.2, 3
-
-  // 4
-  entry: {
-    // polyfill: "babel-polyfill", // 4.1
-    app: ["./js/app.js", "./scss/style.scss"], // 4.2
-  },
-  // 5
-  output: {
-    filename: `./js/${filename(".js")}`, // 5.1, 7.2
-    path: path.resolve(__dirname, "dist"), // 5.2
-    publicPath: "", // 5.3
-  },
-  // 11
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
-    },
-    historyApiFallback: true,
-    open: true,
-    compress: true,
-    hot: true,
-    port: 3000,
-    publicPath: "/",
-  },
-
-  devtool: isProd ? false : "source-map", // 16
-
-  optimization: optimization(), // 17.3
-
-  plugins: [
-    // 8.2
-    new HTMLWebpackPlugin({
-      template: path.resolve(__dirname, "src/index.html"), // 8.3
-      filename: "index.html", // 8.4
-      minify: {
-        // 8.5
-        collapseWhitespace: isProd,
-      },
-    }),
-    new CleanWebpackPlugin(), // 9.2
-    new MiniCssExtractPlugin({
-      // 10.3
-      filename: `./css/${filename(".css")}`,
-    }),
-    new CopyWebpackPlugin({
-      // 12.2
-      patterns: [
-        {
-          from: path.resolve(__dirname, "src/assets"),
-          to: path.resolve(__dirname, "dist/assets"),
-        },
-      ],
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        // 8.6
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        // 10.2
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        // 10.4
-        test: /\.s[ac]ss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: (resourcePath, context) => {
-                return path.relative(path.dirname(resourcePath), context) + "/";
-              },
             },
           },
-          "css-loader",
-          "sass-loader",
-        ],
+        }),
+      ];
+    }
+    return configObj;
+  };
+
+  return {
+    mode: "development", // 6
+
+    context: PATHS.src, // 3
+
+    devtool: isProd ? false : "source-map", // 16
+
+    // 4
+    entry: {
+      app: ["./js/app.js", "./scss/style.scss"],
+    },
+
+    // 5
+    output: {
+      filename: `./js/${filename(".js")}`,
+      path: PATHS.dist,
+      clean: true,
+    },
+
+    // 7
+    devServer: {
+      static: {
+        directory: PATHS.dist,
       },
-      {
-        // 13
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: `img/${filename("[ext]")}`,
+      port: 8080,
+      compress: true,
+      historyApiFallback: true,
+      open: true,
+      hot: true,
+    },
+
+    optimization: optimization(), // 17.3
+
+    plugins: [
+      // new CopyWebpackPlugin({
+      // const CopyWebpackPlugin = require("copy-webpack-plugin");
+      //   patterns: [
+      //     {
+      //       from: PATHS.devAssets,
+      //       to: PATHS.prodAssets,
+      //       noErrorOnMissing: true,
+      //     },
+      //   ],
+      // }),
+
+      // 8.2
+      new HTMLWebpackPlugin({
+        template: PATHS.html,
+        filename: "index.html",
+        minify: {
+          collapseWhitespace: isProd,
         },
-      },
-      {
-        // 14
-        test: /\.(woff2)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: `fonts/${filename("[ext]")}`,
-        },
-      },
-      {
-        // 15
-        test: /\.js$/,
-        exclude: /node_modules/, // 15.1
-        use: ["babel-loader"], // 15.2
-      },
+      }),
+
+      // 9.2
+      new MiniCssExtractPlugin({
+        filename: `./css/${filename(".css")}`,
+      }),
     ],
-  },
+
+    module: {
+      rules: [
+        {
+          // 8.3
+          test: /\.html$/i,
+          loader: "html-loader",
+        },
+
+        {
+          // 9.3
+          test: /\.(s[ac]ss|css)$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+            },
+            {
+              loader: "css-loader",
+              options: { sourceMap: true },
+            },
+            {
+              loader: "postcss-loader",
+              options: { sourceMap: true },
+            },
+            {
+              loader: "resolve-url-loader",
+            },
+            {
+              loader: "sass-loader",
+              options: { sourceMap: true },
+            },
+          ],
+          exclude: "/node_modules",
+        },
+        {
+          // 10.1
+          test: /\.(png|jpe?g|gif)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: `assets/imgs/${filename("[ext]")}`,
+          },
+        },
+        // {
+        //   test: /\.(ico)$/i,
+        //   type: "asset/resource",
+        //   generator: {
+        //     filename: `assets/${filename("[ext]")}`,
+        //   },
+        // },
+        {
+          // 14
+          test: /\.(woff2)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: `fonts/${filename("[ext]")}`,
+          },
+        },
+        {
+          // 15
+          test: /\.js$/,
+          exclude: /node_modules/, // 15.1
+          use: ["babel-loader"], // 15.2
+        },
+      ],
+    },
+  };
 };
